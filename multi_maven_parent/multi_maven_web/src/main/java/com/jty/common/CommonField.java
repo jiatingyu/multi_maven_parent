6ab57e59255e0ee2e3d59038c658c20725e7794b
jiatingyu <jiatingyu94@gmail.com>
2017-10-11 09:33:44 +0800

    公司提交

--- multi_maven_parent/multi_maven_web/src/main/java/com/jty/common/CommonField.java
+++ multi_maven_parent/multi_maven_web/src/main/java/com/jty/common/CommonField.java
@@ -2,7 +2,7 @@ package com.jty.common;
 
 public interface CommonField {
 	String Login_URL="http://localhost:8088/maven/html/login.html";
-	String Login_opreate="http://localhost:8088/maven/permission/login";
+	String Login_opreate="http://192.168.0.103:8088/maven/permission/login";
 	
 	
 }
